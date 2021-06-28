979b14a13454c357f1df9c5e5b46dbc1acdfdd06
Balajivshenoy <balajishenoy8@gmail.com>
2021-06-28 13:18:55 +0530

    LoginController updated

--- Proj/src/main/java/com/FeeManagement/controllersFile/LoginController.java
+++ Proj/src/main/java/com/FeeManagement/controllersFile/LoginController.java
@@ -5,18 +5,42 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.FeeManagement.ServiceFile.UserDaoImpl;
+import com.FeeManagement.entityFile.Login;
+import com.FeeManagement.entityFile.User;
+
 @Controller
 public class LoginController {
 	@Autowired
-//	UserService userService;
-	@RequestMapping(value="/login", method = RequestMethod.GET)
+	UserDaoImpl userDaoImpl;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView showLogin(HttpServletRequest req, HttpServletResponse res) {
-		ModelAndView mv =null;
-//		User user = userService.validateUser(login)
-		return mv;
+		ModelAndView mav = new ModelAndView("login");
+		mav.addObject("login", new Login());
+		return mav;
+	}
+
+	@RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
+	public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
+			@ModelAttribute("login") Login login) {
+		ModelAndView mav = null;
+
+		User user = userDaoImpl.validateUser(login);
+
+		if (null != user) {
+			mav = new ModelAndView("welcome");
+			mav.addObject("username", user.getUserName());
+		} else {
+			mav = new ModelAndView("login");
+			mav.addObject("message", "Username or Password is wrong!!");
+		}
+
+		return mav;
 	}
 }
